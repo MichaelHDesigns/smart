0512c337853873f5c2cfe9a46ac052743fb42fac
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-28 00:52:31 -0400

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,13 @@ const CharityFactory = artifacts.require("CharityFactory");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(CampaignFactory);
-  await deployer.deployer(CharityFactory);
+  await deployer.deploy(CharityFactory);
 
   if (network === 'development') {
     await deployer.deploy(CampaignWallet, [accounts[1], accounts[2]], [50, 50], ['FoodCo', 'BuildCo'], ['Food', 'Construction']);
-    const factory = await CampaignFactory.deployed();
-    const factory = await CharityFactory.deployed();
+    const campaignFactory = await CampaignFactory.deployed();
+    const charityFactory = await CharityFactory.deployed();
     const now = Math.floor(new Date().getTime() / 1000);
-    await factory.createCampaign(CampaignWallet.address, 1e18.toString(), now, now + 100000, 'Sample title', 'Sample description');
+    await campaignFactory.createCampaign(CampaignWallet.address, 1e18.toString(), now, now + 100000, 'Sample title', 'Sample description');
   }
-};
+}
