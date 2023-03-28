65b1f1baeaa37959c4c2365afc8e5f9264f6be70
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-28 00:45:53 -0400

    Update Charity.sol

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -3,57 +3,51 @@ pragma solidity ^0.8.0;
 
 contract CharityFactory {
     address[] public deployedCharities;
-    
+
     function createCharity() public {
-        address newCharity = new Charity( msg.sender );
+        address newCharity = address(new Charity(msg.sender));
         deployedCharities.push(newCharity);
     }
-    
-    function getDeployedCharity() public view returns (address[]) {
+
+    function getDeployedCharity() public view returns (address[] memory) {
         return deployedCharities;
     }
 }
 
+contract Charity {
+    event CharityEvent(address indexed from, string message, string username, uint value);
 
-contract Charity{
-  event CharityEvent(
-    address _from,
-    string _message,
-    string _username,
-    uint _value
-  );
-  
-  struct Donator {
-    string message;
-    string username;
-    uint value;
-}
-
-address public organization;
-Donator[] public donators;
-uint public charityCount;
-
-     function Charity( address creator) public {
-          organization = creator;
+    struct Donator {
+        string message;
+        string username;
+        uint value;
     }
-  
-     function getSummary() public view returns ( uint, uint, uint ) {
-          return (
-              this.balance,
-              donators.length,
-              charityCount
-          );
-      }
 
-    function contributeMessage(string message, string username, uint value) public payable {
-      charityCount++;
-      
-        emit CharityEvent(msg.sender, message, username, value);
+    address public organization;
+    Donator[] public donators;
+    uint public charityCount;
+
+    constructor(address creator) {
+        organization = creator;
+    }
+
+    function getSummary() public view returns (uint balance, uint numDonators, uint numCharities) {
+        return (
+            address(this).balance,
+            donators.length,
+            charityCount
+        );
+    }
+
+    function contributeMessage(string memory message, string memory username) public payable {
+        charityCount++;
+
+        emit CharityEvent(msg.sender, message, username, msg.value);
         Donator memory newDonator = Donator({
-        message: message,
-        username: username,
-        value: value
-    });
+            message: message,
+            username: username,
+            value: msg.value
+        });
         donators.push(newDonator);
     }
 }
