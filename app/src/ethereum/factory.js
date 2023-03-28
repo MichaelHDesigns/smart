dbb73964ec3089a2a15873cd7cdfac41dbc3b665
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-28 00:54:13 -0400

    Update factory.js

--- app/src/ethereum/factory.js
+++ app/src/ethereum/factory.js
@@ -4,7 +4,7 @@ import CharityCampaign from './build/Charity.json';
 //pre-configured instance for our contract
 const instance = new web3.eth.Contract(
   JSON.parse(CharityCampaign.interface),
-  '0x20c30c55bbf5a6d3776526dcc17b33480d545177'
+  '0x04F81c25874d3051A28693a395b8D790c26336Bc'
 );
 
 export default instance;
