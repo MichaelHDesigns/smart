651cccb51bb69ec56259a891de707f9c6b82d36f
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-28 02:20:43 -0400

    Update charity.js

--- app/src/ethereum/charity.js
+++ app/src/ethereum/charity.js
@@ -1,5 +1,5 @@
 import web3 from './web3';
-import Charity from './build/Charity.json';
+import Charity from '../../build/contracts/Charity.json';
 
 export default ( address ) => {
   return new web3.eth.Contract(
