c4751bd0936b6fc5a4b87e839692059df6aac1b6
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-28 04:05:14 -0400

    Update DonateForm.vue

--- app/src/components/DonateForm.vue
+++ app/src/components/DonateForm.vue
@@ -65,7 +65,7 @@ export default {
         }
         const accounts = await web3.eth.getAccounts();
         this.loading = true;
-        this.errorMessage = '';
+        this.errorMessage = '',
         await charity.methods.contributeMessage(description, name, web3.utils.toWei(value, 'ether'))
         .send({
           from: accounts[0],
@@ -95,8 +95,8 @@ export default {
       }
     },
     onClose(e) {
-      if (this.props.onClose) {
-        this.props.onClose(e)
+      if (this.onClose) { // check if onClose exists
+        this.onClose(e)
       }
     }
   }
